63c7ee85f4661518cfe35078939584f2edf65c7d
Nicolae Darie-Nistor <memedarie@gmail.com>
2023-07-05 21:47:37 +0200

    Widgets

---
+++ MotiqWidgets/MotiqWidgetsBundle.swift
@@ -0,0 +1,16 @@
+//
+//  MotiqWidgetsBundle.swift
+//  MotiqWidgets
+//
+//  Created by Darie-Nistor Nicolae on 05.07.2023.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MotiqWidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        MotiqWidgets()
+    }
+}
